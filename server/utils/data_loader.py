ab2072793552ba15036e25ad5b8cac4c3769b5f2
cinno <fmlong2000@gmail.com>
2024-10-30 20:48:46 +0800

    New data_loader

--- server/utils/data_loader.py
+++ server/utils/data_loader.py
@@ -1,5 +1,5 @@
 import re
-
+import pandas as pd
 translator = {
     'g': 'Food',
     'b': 'Industry',
@@ -21,42 +21,34 @@ translator = {
     'N': 'ArbitraryWorld',
     'F': 'Fleet'
 }
-
+translator_donation = {
+    'g': 'FoodDonation',
+    'b': 'IndustryDonation',
+    'w': 'CultureDonation',
+    'B': 'InformationDonation',
+    'T': 'BiotechDonation',
+    'Y': 'EnergyDonation',
+    'U': 'HypertechDonation',
+    '*': 'ShipDonation',
+    '$': 'ScoreDonation',
+    's': 'ArbitarySmallDonation',
+    'L': 'ArbitaryBigDonation',
+    'A': 'WildBigDonation',
+    'a': 'WildSmallDonation',
+    'J': 'JungleDonation',
+    'D': 'DesertDonation',
+    'W': 'WaterDonation',
+    'I': 'IceDonation',
+    'N': 'ArbitraryWorldDonation',
+    'F': 'FleetDonation'
+}
 def factory_gen(fac):
     input=re.match('.*(?=➪|→)',fac).group()
     input_items={
-                    "Food": 0,
-                    "Culture": 0,
-                    "Industry": 0,
-                    "Information": 0,
-                    "Biotech": 0,
-                    "Energy": 0,
-                    "Hypertech": 0,
-                    "Ship": 0,
-                    "Score": 0
                 }
     output_items={
-                "Food": 0,
-                "Culture": 0,
-                "Industry": 0,
-                "Information": 0,
-                "Biotech": 0,
-                "Energy": 0,
-                "Hypertech": 0,
-                "Ship": 0,
-                "Score": 0
             }
-    donation_items={
-                "Food": 0,
-                "Culture": 0,
-                "Industry": 0,
-                "Information": 0,
-                "Biotech": 0,
-                "Energy": 0,
-                "Hypertech": 0,
-                "Ship": 0,
-                "Score": 0
-            }
+
     for i in input:
         matched = False
         for key in translator.keys():
@@ -66,130 +58,37 @@ def factory_gen(fac):
                 break                      
         if not matched:
             raise NameError(f'i=={i},{fac}')
-            #print(input_items)
-            
+                    
     output=re.search('(?=➪|→).+',fac).group()[1:]
     if re.search('\+',output):
         output_no_donation=re.search('.*(?=\+)',output).group()
         output_donation=re.search('(?=\+).*',output).group()[1:]
-
-        for i in output_no_donation:
-            if i =='g':
-                output_items['Food'] = output_items.get('Food',0) + 1
-            elif i =='b':
-                output_items['Industry']+=1
-            elif i=='w':
-                output_items["Culture"]+=1
-            elif i=='B':
-                output_items['Information']+=1
-            elif i=='T':
-                output_items['Biotech']+=1
-            elif i=='Y':
-                output_items['Energy']+=1
-            elif i=='U':
-                output_items['Hypertech']+=1
-            elif i=="*":
-                output_items['Ship']+=1
-            elif i=='$':
-                output_items['Score']+=1
-            elif i=='A':
-                if 'WildBig' in output_items.keys():
-                    output_items['WildBig']+=1
-                else:
-                    output_items['WildBig']=1                    
-            elif i=='a':
-                if 'WildSmall' in output_items.keys():
-                    output_items['WildSmall']+=1
-                else:
-                    output_items['WildSmall']=1   
-            elif i=='J':
-                output_items['jungle']=1
-            elif i=='D':
-                output_items['desert']=1
-            elif i=='W':
-                output_items['water']=1
-            elif i=='I':
-                output_items['ice']=1      
-            else:  
-                 raise NameError(f'i=={i},{fac}')
         for i in output_donation:
-            if i =='g':
-                donation_items['Food']+=1
-            elif i =='b':
-                donation_items['Industry']+=1
-            elif i=='w':
-                donation_items["Culture"]+=1
-            elif i=='B':
-                donation_items['Information']+=1
-            elif i=='T':
-                donation_items['Biotech']+=1
-            elif i=='Y':
-                donation_items['Energy']+=1
-            elif i=='U':
-                donation_items['Hypertech']+=1
-            elif i=="*":
-                donation_items['Ship']+=1
-            elif i=='$':
-                donation_items['Score']+=1
-            elif i=='A':
-                if 'WildBig' in donation_items.keys():
-                    donation_items['WildBig']+=1
-                else:
-                    donation_items['WildBig']=1                    
-            elif i=='a':
-                if 'WildSmall' in donation_items.keys():
-                    donation_items['WildSmall']+=1
-                else:
-                    donation_items['WildSmall']=1    
-            elif i=='J':
-                donation_items['jungle']=1
-            elif i=='D':
-                donation_items['desert']=1
-            elif i=='W':
-                donation_items['water']=1
-            elif i=='I':
-                donation_items['ice']=1   
-            else:
+            matched = False
+            for key in translator_donation.keys():
+                if i==key:
+                    output_items[translator_donation[key]] = output_items.get(translator_donation[key], 0) + 1
+                    matched = True
+                    break                      
+            if not matched:
                 raise NameError(f'i=={i},{fac}')
     else:
         output_no_donation=output
-        for i in output_no_donation:
-            if i =='g':
-                output_items['Food']+=1
-            elif i =='b':
-                output_items['Industry']+=1
-            elif i=='w':
-                output_items["Culture"]+=1
-            elif i=='B':
-                output_items['Information']+=1
-            elif i=='T':
-                output_items['Biotech']+=1
-            elif i=='Y':
-                output_items['Energy']+=1
-            elif i=='U':
-                output_items['Hypertech']+=1
-            elif i=="*":
-                output_items['Ship']+=1
-            elif i=='$':
-                output_items['Score']+=1     
-            elif i=='A':
-                if 'WildBig' in output_items.keys():
-                    output_items['WildBig']+=1
-                else:
-                    output_items['WildBig']=1                    
-            elif i=='a':
-                if 'WildSmall' in output_items.keys():
-                    output_items['WildSmall']+=1
-                else:
-                    output_items['WildSmall']=1      
-            else:
-                raise NameError(f'i=={i},{fac}')
-    return output_items,donation_items,input_items
-    import pandas as pd
+    for i in output_no_donation:
+        matched = False
+        for key in translator.keys():
+            if i==key:
+                output_items[translator[key]] = output_items.get(translator[key], 0) + 1
+                matched = True
+                break                      
+        if not matched:
+            raise NameError(f'i=={i},{fac}')
+    return output_items,input_items
+
 def factory_from_csv(fac):
     factory={}
     factory["name"]=f'{fac['Faction']}_{fac['Front Name']}'
-    output_items,donation_items,input_items=factory_gen(fac['Front Factory'])
+    output_items,input_items=factory_gen(fac['Front Factory'])
     factory["output_items"]=output_items
     factory['input_items']=input_items
     factory['donation_items']=donation_items
@@ -211,11 +110,11 @@ def factory_from_csv(fac):
         upgrade.append(upgrade_substance)
     else:
         upgrade.append(f'{fac['Faction']}_{fac['Upgrade1']}')
-    factory['feature']['properties']['upgrade_requirements']=upgrade
+    factory['feature']['properties']['upgrade_cost']=upgrade
 
     back_factory={}
     back_factory["name"]=f'{fac['Faction']}_{fac['Back Name']}'
-    output_items,donation_items,input_items=factory_gen(fac['Back Factory'])
+    output_items,input_items=factory_gen(fac['Back Factory'])
     back_factory["output_items"]=output_items
     back_factory['input_items']=input_items
     back_factory['donation_items']=donation_items
